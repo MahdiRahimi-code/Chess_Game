3eee00fbe081c64b92c37159cd7d8db5140825b9
unknown <mahdirahimi1250@gmail.com>
2023-10-08 23:12:31 +0330

    edit draw_pieces

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ timer = py.time.Clock()
 text = py.font.Font(None, 30)
 
 
+
 def create_board():
     # black lines around the board
     py.draw.rect(screen, 'black', (198, 67, 564, 566), 3)
@@ -65,35 +66,101 @@ def load_images():
     black_soldier = py.image.load("C:\\Users\\Acer\\Desktop\\Python\\Chess\\images\\black soldier.png")
     black_soldier = py.transform.scale(black_soldier, (60, 60))
 
+
     
+
+black_pieces = ['castle', 'horse', 'elephant' ,'queen', 'king', 'elephant', 'horse', 'castle',
+                'soldier', 'soldier', 'soldier', 'soldier', 'soldier', 'soldier', 'soldier', 'soldier']
+black_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                    (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+white_pieces = ['castle', 'horse', 'elephant' ,'queen', 'king', 'elephant', 'horse', 'castle',
+                'soldier', 'soldier', 'soldier', 'soldier', 'soldier', 'soldier', 'soldier', 'soldier']
+white_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                    (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+
 
 
 def draw_pieces():
-    screen.blit(white_castle, (200, 565))
-    screen.blit(white_horse, (275, 565))
-    screen.blit(white_elephant, (345, 565))
-    screen.blit(white_queen, (415, 565))
-    screen.blit(white_king, (485, 565))
-    screen.blit(white_elephant, (555, 565))
-    screen.blit(white_horse, (625, 565))
-    screen.blit(white_castle, (695, 565))
-    for i in range(8):
-        screen.blit(white_soldier, (200+ 70*i, 495))
+    for i in range(len(white_pieces)):
+        if white_pieces[i] == 'soldier':
+            screen.blit(white_soldier, (200 + white_locations[i][0]*70, 70 + white_locations[i][1]*70))
 
-    screen.blit(black_castle, (200, 75))
-    screen.blit(black_horse, (275, 75))
-    screen.blit(black_elephant, (345, 75))
-    screen.blit(black_queen, (415, 75))
-    screen.blit(black_king, (485, 75))
-    screen.blit(black_elephant, (555, 75))
-    screen.blit(black_horse, (625, 75))
-    screen.blit(black_castle, (695, 75))
-    for i in range(8):
-        screen.blit(black_soldier, (200+ 70*i, 145))
+        elif white_pieces[i] == 'castle':
+            screen.blit(white_castle, (200 + white_locations[i][0]*70, 70 + white_locations[i][1]*70))
+        
+        elif white_pieces[i] == 'horse':
+            screen.blit(white_horse, (200 + white_locations[i][0]*70, 70 + white_locations[i][1]*70))
+
+        elif white_pieces[i] == 'elephant':
+            screen.blit(white_elephant, (200 + white_locations[i][0]*70, 70 + white_locations[i][1]*70))
+        
+        elif white_pieces[i] == 'queen':
+            screen.blit(white_queen, (200 + white_locations[i][0]*70, 70 + white_locations[i][1]*70))
+        
+        elif white_pieces[i] == 'king':
+            screen.blit(white_king, (200 + white_locations[i][0]*70, 70 + white_locations[i][1]*70))
+        
+        if turn == "white":
+            if selected_piece[0] != -1:
+                x = white_locations[selected_piece[0]][0]
+                y = white_locations[selected_piece[0]][1]
+                py.draw.rect(screen, 'red', (200+(x*70), 70+(y*70), 70, 70), 3)
+        
+    
+    for i in range(len(black_pieces)):
+        if black_pieces[i] == 'soldier':
+            screen.blit(black_soldier, (200 + black_locations[i][0]*70, 70 + black_locations[i][1]*70))
+
+        elif black_pieces[i] == 'castle':
+            screen.blit(black_castle, (200 + black_locations[i][0]*70, 70 + black_locations[i][1]*70))
+        
+        elif black_pieces[i] == 'horse':
+            screen.blit(black_horse, (200 + black_locations[i][0]*70, 70 + black_locations[i][1]*70))
+
+        elif black_pieces[i] == 'elephant':
+            screen.blit(black_elephant, (200 + black_locations[i][0]*70, 70 + black_locations[i][1]*70))
+        
+        elif black_pieces[i] == 'queen':
+            screen.blit(black_queen, (200 + black_locations[i][0]*70, 70 + black_locations[i][1]*70))
+        
+        elif black_pieces[i] == 'king':
+            screen.blit(black_king, (200 + black_locations[i][0]*70, 70 + black_locations[i][1]*70))
+
+        if turn == "black":
+            if selected_piece[0] != -1:
+                x = white_locations[selected_piece[0]][0]
+                y = white_locations[selected_piece[0]][1]
+                py.draw.rect(screen, 'blue', (200+(x*70), 70+(y*70), 70, 70), 3)        
 
 
+allowed_moves = []
 
+
+def check_moves(selected_piece):
+    if selected_piece[1] == 'soldier':
+        check_soldier_moves(selected_piece)
+    
+        
+def check_soldier_moves(selected_piece):
+    if turn == 'white':
+        if selected_piece[0][1] == 6:
+            allowed_moves.append((selected_piece[0][0], selected_piece[0][1]-1))
+            allowed_moves.append((selected_piece[0][0], selected_piece[0][1]-2))
+
+
+def draw_allowed_moves():
+    for i in range(len(allowed_moves)):
+        py.draw.rect(screen, 'gold', (200+ 70*allowed_moves[i][0], 70+ 70*allowed_moves[i][1], 70, 70))
+
+
+# white or black
+turn = 'white'
+selected_piece = [-1, '']
+
+# variable for disselecting piece
+alter = [-1 , -1]
 running = True
+
 while running:
     timer.tick(60)
     screen.fill((115, 73, 31))
@@ -101,11 +168,49 @@ while running:
     load_images()
     draw_pieces()
 
+    if selected_piece[0] != -1:
+        check_moves(selected_piece)
+        draw_allowed_moves()
+
 
 
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
+        if event.type == py.MOUSEBUTTONDOWN and event.button == 1:
+            select_x = (event.pos[0] - 200) // 70
+            select_y = (event.pos[1] - 70) // 70
+            selected_coords = (select_x, select_y)
+
+            if (turn == "white"):
+                if (selected_coords in white_locations):
+                    index = white_locations.index(selected_coords)
+                    selected_piece = [index, white_pieces[index]]
+                    if alter == selected_piece:
+                        alter = [-1, -1]
+                        selected_piece = [-1, '']
+
+                    alter = selected_piece
+
+                if selected_coords in allowed_moves:
+                        white_locations[index] = selected_coords
+                        selected_piece = [-1, '']
+                        turn = 'black'
+                         
+
+
+                   
+
+                    
+
+
+                    
+                    
+
+            
+
+                    
+            
 
 
     py.display.flip()
